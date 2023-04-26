6d968c72bab3ecb0eedd8dc0df08802ea904305f
dickson anyaele <dicksonanyaele1234@gmail.com>
2023-04-26 10:59:20 -1200

    added backend files

---
+++ backend/src/app.ts
@@ -0,0 +1,34 @@
+
+import express, { RequestHandler, } from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+
+class App {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routerSetup();
+  }
+
+  private config() {
+
+    this.app.use(logger('dev'));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(cookieParser());
+    this.app.use(express.static(path.join(__dirname, 'public')));
+  }
+
+  private routerSetup() {
+    this.app.use('/', indexRouter);
+  }
+
+}
+
+export default new App().app;
+
