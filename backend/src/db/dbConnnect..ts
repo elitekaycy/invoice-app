d45eac3829b21ea2f2220f22ccb6508061f7854b
dickson anyaele <dicksonanyaele1234@gmail.com>
2023-04-30 08:38:01 -1200

    fixed the create route

--- backend/src/db/dbConnnect..ts
+++ backend/src/db/dbConnnect..ts
@@ -1,18 +1,18 @@
-import { Client, Pool } from 'pg'
-import dotenv from 'dotenv'
+import { Client, Pool } from "pg";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export const client = new Pool({
-    host: 'localhost',
-    port: 5432,
-    database: 'invoicedb',
-    user: 'postgres',
-    password: 'root',
-})
+  host: "localhost",
+  port: 5432,
+  database: "invoicedb",
+  user: "postgres",
+  password: "root",
+});
 
 const createTableQueries = [
-    `
+  `
       CREATE TABLE IF NOT EXISTS invoice (
         id SERIAL PRIMARY KEY NOT NULL,
         clientName varchar(100) NOT NULL,
@@ -21,11 +21,11 @@ const createTableQueries = [
         paymentDue varchar(50) NOT NULL,
         paymentTerms varchar(100) NOT NULL,
         description text NOT NULL,
-        created_at date,
+        created_at date DEFAULT CURRENT_DATE,
         total money NOT NULL
       )
     `,
-    `
+  `
       CREATE TABLE IF NOT EXISTS address (
         id SERIAL PRIMARY KEY UNIQUE,
         street varchar(100) NOT NULL,
@@ -33,45 +33,44 @@ const createTableQueries = [
         client boolean NOT NULL,
         postcode varchar(100),
         country varchar(60),
-        reference_id SERIAL REFERENCES invoice(id) ON DELETE CASCADE
+        invoice_id SERIAL REFERENCES invoice(id) ON DELETE CASCADE
       )
     `,
-    `
+  `
      CREATE TABLE IF NOT EXISTS items (
       id SERIAL PRIMARY KEY UNIQUE,
       name varchar(100) NOT NULL,
       quantity integer NOT NULL,
-      price integer NOT NULL,
-      reference_id SERIAL REFERENCES invoice(id) ON DELETE CASCADE
+      price money NOT NULL,
+      invoice_id SERIAL REFERENCES invoice(id) ON DELETE CASCADE
     )
-  `
-  ];
-  
+  `,
+];
 
 // Connect to the PostgreSQL server using the pool
 client.connect((err, client, release) => {
-    if (err) {
-      console.error('Failed to connect to the database', err);
-      return;
-    }
-    console.log('Connected to the database', process.env.DB_PORT);
-  
-    // Iterate over the array of SQL queries and execute each one
-    createTableQueries.forEach((query) => {
-      client.query(query, (err, result) => {
-        if (err) {
-          console.error('Failed to create table', err);
-          return;
-        }
-        console.log('Table created successfully');
-      });
+  if (err) {
+    console.error("Failed to connect to the database", err);
+    return;
+  }
+  console.log("Connected to the database", process.env.DB_PORT);
+
+  // Iterate over the array of SQL queries and execute each one
+  createTableQueries.forEach((query) => {
+    client.query(query, (err, result) => {
+      if (err) {
+        console.error("Failed to create table", err);
+        return;
+      }
+      console.log("Table created successfully");
     });
-  
-    // Release the client back to the pool
-    release();
   });
+
+  // Release the client back to the pool
+  release();
+});
 
 // address table - street, city, postcode, country, reference_id, client|sender - boolean
 // client table -  clientName, clientEmail, reference_id,
-// reference table - created at, payment due, descriptions, paymentTerms, status, total, clientName, clientEmail id 
-// items table - name, quantity, price, total, reference_id 
+// reference table - created at, payment due, descriptions, paymentTerms, status, total, clientName, clientEmail id
+// items table - name, quantity, price, total, reference_id
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,16 +1,14 @@
-import express, {Request, Response} from 'express';
-import { getLogger } from '@/utils/loggers';
-import { InvoiceGetFromClientType } from '@/types';
-import { client } from '../db/dbConnnect.'
+import express, { Request, Response } from "express";
+import { getLogger } from "@/utils/loggers";
+import { InvoiceGetFromClientType } from "@/types";
+import { client } from "../db/dbConnnect.";
 const router = express.Router();
-const logger = getLogger('INDEX_ROUTE');
+const logger = getLogger("INDEX_ROUTE");
 
 /* GET home page. */
-router.get('/', function (_req, res, _next) {
-  res.json({ data: 'invoice router '})
+router.get("/", function (_req, res, _next) {
+  res.json({ data: "invoice router " });
 });
-
-const InvoiceDB = []
 
 // create invoice router
 /* 
@@ -20,72 +18,87 @@ const InvoiceDB = []
   4. sends back 201 message with created to be true
 */
 
-router.post ('/create', function(_req: Request, res: Response, _next){
-      try {
-        const newInvoice: InvoiceGetFromClientType  = _req?.body
-        console.log("the request body ", _req?.body)
+router.post("/create", function (_req: Request, res: Response, _next) {
+  try {
+    const newInvoice: InvoiceGetFromClientType = _req?.body;
 
-        client.query(`INSERT INTO invoice 
+    client.query(
+      `INSERT INTO invoice 
         (clientName, clientEmail, status, paymentDue, paymentTerms, description, total) 
-        VALUES($1, $2, $3, $4, $5 ,$6, $7) RETURNING id`, 
-        [newInvoice?.clientName, 
-         newInvoice?.clientEmail, 
-         newInvoice?.status, 
-         newInvoice?.paymentDue, 
-         newInvoice?.paymentTerms, 
-         newInvoice?.projectDescription,
-         newInvoice?.total
-        ], (err, results) => {
-          if(err) {
-            throw new Error(`${err?.name}: ${err?.message}`)
-          }
+        VALUES($1, $2, $3, $4, $5 ,$6, $7) RETURNING id`,
+      [
+        newInvoice?.clientName,
+        newInvoice?.clientEmail,
+        newInvoice?.status,
+        newInvoice?.paymentDue,
+        newInvoice?.paymentTerms,
+        newInvoice?.projectDescription,
+        newInvoice?.total,
+      ],
+      (err, results) => {
+        if (err) {
+          throw new Error(`${err?.name}: ${err?.message}`);
+        }
 
-          const invoiceId = results?.rows[0]?.id
-          console.log("invoice id is ", results)
-          // insert both address - if error delete reference
-          // insert items - if error delete reference
-          const invoiceValues = [
-            [newInvoice?.clientAddress?.country, newInvoice?.clientAddress?.city, 
-              newInvoice?.clientAddress?.postCode, newInvoice?.clientAddress?.streetAddress, 
-              true, invoiceId],
-            [
-              newInvoice?.senderAddress?.country, newInvoice?.senderAddress?.city, 
-                newInvoice?.senderAddress?.postCode, newInvoice?.senderAddress?.streetAddress, 
-                false, invoiceId
-            ]
-          ]
+        const invoiceId = results?.rows[0]?.id;
 
-          for (let i = 0; i < invoiceValues?.length; i++){
-            client.query(`INSERT INTO address (country, city, postcode, street, client, reference_id) VALUES ($1, $2, $3, $4, $5, $6)`, invoiceValues[i], 
+        // insert both address - if error delete reference
+        // insert items - if error delete reference
+        const invoiceValues = [
+          [
+            newInvoice?.clientAddress?.country,
+            newInvoice?.clientAddress?.city,
+            newInvoice?.clientAddress?.postCode,
+            newInvoice?.clientAddress?.streetAddress,
+            true,
+            invoiceId,
+          ],
+          [
+            newInvoice?.senderAddress?.country,
+            newInvoice?.senderAddress?.city,
+            newInvoice?.senderAddress?.postCode,
+            newInvoice?.senderAddress?.streetAddress,
+            false,
+            invoiceId,
+          ],
+        ];
+
+        for (let i = 0; i < invoiceValues?.length; i++) {
+          client.query(
+            `INSERT INTO address (country, city, postcode, street, client, invoice_id) VALUES ($1, $2, $3, $4, $5, $6)`,
+            invoiceValues[i],
             (err, result) => {
-              if(err){
+              if (err) {
                 // delete invoice here...
-                throw new Error(`${err?.name}: ${err?.message}`)
+                throw new Error(`${err?.name}: ${err?.message}`);
               }
             }
-            )
-          }
+          );
+        }
 
-          newInvoice?.items?.forEach(item => {
-            client.query(`INSERT INTO items (name, quantity, price, reference_id) VALUES ($1, $2, $3, $4)`, [
-              item?.itemName, item?.itemQuantity, item?.itemPrice, invoiceId
-            ], (err, result) => {
-              if(err) {
+        newInvoice?.items?.forEach((item) => {
+          client.query(
+            `INSERT INTO items (name, quantity, price, invoice_id) VALUES ($1, $2, $3, $4)`,
+            [item?.itemName, item?.itemQuantity, item?.itemPrice, invoiceId],
+            (err, result) => {
+              if (err) {
                 // delete invoice here
-                throw new Error(`${err?.name}: ${err?.message}`)
+                throw new Error(`${err?.name}: ${err?.message}`);
               }
+            }
+          );
+        });
 
-              return res.status(201).send('created successfully')
-            })
-          })
-
-        })
-
+        return res.status(201).send("created successfully");
       }
-      catch(err){
-        res.status(400).send(`error occured ${err}`)
-      }
+    );
+  } catch (err) {
+    res.status(400).send(`error occured ${err}`);
+  }
+});
 
-})
+// GET TABLE DATA
+// use data id to get all the data that is relating to the invoice data -
+// make sure it corresponds with the invoice_data types
 
 export default router;
